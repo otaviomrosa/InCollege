715af8c341f108dfdf2be1efe8337ab72a3e9561
Otavio Rosa <otavio.exec@gmail.com>
2025-10-08 13:18:23 -0400

    Fix message of invalid option managing requests
    The program used to say "skipping this request" which is not accurate,
    since the user can try again.
    
    Request from: eve
    1. Accept
    2. Reject
    Enter your choice: g
    
    ERROR: Invalid choice, skipping this request.
    
    Request from: eve
    1. Accept
    2. Reject

--- InCollege.cob
+++ InCollege.cob
@@ -9,7 +9,7 @@
       file-control.
 *>    Define three files: input-file, output-file, and accounts-file and assign them to text files
 *>    The accounts-file will be used to store user account information
-          select input-file assign to 'InCollege-Input.txt'
+          select input-file assign to KEYBOARD
               organization is line sequential.
           select output-file assign to 'InCollege-Output.txt'
               organization is line sequential.
@@ -912,7 +912,7 @@
            exit paragraph.
        
        *> ========= View My Pending Requests =========
-        view-my-pending-requests
+      view-my-pending-requests.
         move 0 to ws-list-count
 
 
@@ -1072,7 +1072,7 @@
                    end-if
 
                    if ws-action-choice not = '1' and ws-action-choice not = '2'
-                       move "Invalid choice, skipping this request." to ws-message
+                       move "Invalid choice, try again." to ws-message
                        perform display-error
                    end-if
                end-if
